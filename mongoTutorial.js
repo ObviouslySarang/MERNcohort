2ffdb094b9868e6a7b3ba68b2be5251d4332923e
Sarang Dixit <2003krishnadixit@gmail.com>
2024-05-26 04:47:53 +0530

    basic mongo connection estabalished

--- mongoTutorial.js
+++ mongoTutorial.js
@@ -5,15 +5,15 @@
 
 const mongoose = require("mongoose");
 
-//mongoose.connect("mongodb+srv://userappnew:ebNznJitjMBU2oUr@cluster0.oqrqo4f.mongodb.net/")
-mongoose.connect("mongodb://admin:1234@cluster0.oqrqo4f.mongodb.net:27017/")
-const Users = mongoose.model("users",
-    {name:String,
-     password:String,
-     Firstname:String
+mongoose.connect("mongodb+srv://ObviouslySarang:1234@cluster0.b8cmdfq.mongodb.net/wow")
+const User = mongoose.model("Users",
+    {
+            name:String,
+            password:String,
+            Firstname:String
     });
 
-const user = new Users ({
+const user = new User ({
     name:"Sarang Dixit",
     password:"2442",
     Firstname:"Sarang"
