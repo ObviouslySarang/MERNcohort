cfcc3a45b462afbb48a7aa95f1c9206e4279ddb1
ObviouslySarang <2003krishnadixit@gmail.com>
2025-01-30 13:45:06 +0530

    todo dom

--- mongoTutorial.js
+++ mongoTutorial.js
@@ -2,45 +2,23 @@
 // endpoint 1 - /signup , where the user give { username , password , firstname }
 // endpoint 2 - /signin , which checks if the user exists or not , give token {jwt} if exists
 // endpoint 3 - /users , where we expect a header and hit the database to fetch the user being asked for 
-const express = require("express");
+
 const mongoose = require("mongoose");
 
-const app = express();
-
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect("mongodb+srv://ObviouslySarang:1234@cluster0.b8cmdfq.mongodb.net/wow")
-
-const User = mongoose.model("Users",
-{       Firstname:String,
-        password:String,
-        name:String
-});
-app.post("/signup",async function(req,res){
-
-    const username = req.body.Firstname;
-    const password = req.body.password;
-    const name = req.body.name;
-
-    const existingUser = await User.findOne({Firstname:username});
-
-    if (existingUser){
-        return res.status(400).send("user already exists");
-    }
-    const user = new User ({
-        Firstname:username,
-        name:name,
-        password:password
+//mongoose.connect("mongodb+srv://userappnew:ebNznJitjMBU2oUr@cluster0.oqrqo4f.mongodb.net/")
+mongoose.connect("mongodb+srv://sarang:F$**Akk8F#oU2p3863fto@cluster0.aldmcjq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+const Users = mongoose.model("users",
+    {name:String,
+     password:String,
+     Firstname:String
     });
-   
-    user.save();
 
-    res.json({"msg":"user created successfully"});
-})
+const user = new Users ({
+    name:"Sarang Dixit",
+    password:"2442",
+    Firstname:"Sarang"
+});
 
-app.listen(5000);
-
-
+user.save();
 
 
