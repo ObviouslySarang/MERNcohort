0c21a3604bd9c1c8aa865d01db2f380fe73cc831
ObviouslySarang <2003krishnadixit@gmail.com>
2025-03-08 13:29:10 +0530

    final counter props

--- class4-2/react-tut-vite/counter-react/src/App.jsx
+++ class4-2/react-tut-vite/counter-react/src/App.jsx
@@ -4,15 +4,22 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0)
 
-  function onclickHandler(){
-    setCount(count+1) ;
-  }
+  
   return (
       <div>
         
-        <button onClick={onclickHandler}> GINTI {count}</button>
+       <CustomButton count={count} setCount = {setCount}></CustomButton>
        </div>
   )
+}
+
+function CustomButton(props){
+  function onclickHandler(){
+    props.setCount(props.count+1) ;
+  }
+  return <button onClick={onclickHandler}>
+    Counter {props.count}
+  </button>
 }
 
 export default App
