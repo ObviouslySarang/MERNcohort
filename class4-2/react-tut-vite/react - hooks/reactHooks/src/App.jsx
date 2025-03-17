17464db6cd4b751d3e090bc8aa2a72a36aad4d2a
ObviouslySarang <2003krishnadixit@gmail.com>
2025-03-17 15:38:55 +0530

    setinterval , to send req every 10 sec

--- class4-2/react-tut-vite/react - hooks/reactHooks/src/App.jsx
+++ class4-2/react-tut-vite/react - hooks/reactHooks/src/App.jsx
@@ -1,18 +1,51 @@
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 
 
 
  export function App() {
+   const [todos, setTodos] = useState([]);
+   
+   useEffect(() => {
+    setInterval(() => {
+      console.log('hello')
+      fetch('https://sum-server.100xdevs.com/todos')
+      .then(async(response) => {
+                 const data = await response.json()
+                 setTodos(data)})
+    }
+    , 10000)
+  }
+  , [])
+    
   
+    
   return (
-    <>
-    {/* this is the ugly way to pass a component inside another comp  <CardWrapper innercomp = {<TextComp/>}/> */}
-    {/* <CardWrapper>
-      hi there 
-    </CardWrapper> */}
-    {/* this is the right way , and this can be accessed through "children " variable  */}
-    {/* that is CHILDREN variable contains all the things wrapped by the outer component  */}
-      </>
+
+    <div>
+      {todos.map((todo) => {
+        return <Todo title = {todo.title} desc = {todo.desc}/>
+      }
+      )}
+    </div>
+    
+    // {/* this is the ugly way to pass a component inside another comp  <CardWrapper innercomp = {<TextComp/>}/> */}
+    // {/* <CardWrapper>
+    //   hi there 
+    // </CardWrapper> */}
+    // {/* this is the right way , and this can be accessed through "children " variable  */}
+    // {/* that is CHILDREN variable contains all the things wrapped by the outer component  */}
+
+      
+  )
+}
+function Todo({title,desc}){
+  
+  return(
+    <div>
+      <h1>{title}</h1>
+      <p>{desc}</p>
+    </div>
   )
 }
 // function TextComp(){
