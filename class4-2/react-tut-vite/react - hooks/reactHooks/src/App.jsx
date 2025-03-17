4a48a18c1fb1e95925f80f0314fdc0b47748af11
ObviouslySarang <2003krishnadixit@gmail.com>
2025-03-17 15:06:43 +0530

    wrapper component

--- class4-2/react-tut-vite/react - hooks/reactHooks/src/App.jsx
+++ class4-2/react-tut-vite/react - hooks/reactHooks/src/App.jsx
@@ -1,47 +1,34 @@
 import React, { useState } from 'react';
 
-let count = 4;
+
 
  export function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      title: 'todo1',
-      desc: 'description1'
-    },
-    {
-      id:2,
-      title: 'todo2',
-      desc: 'description2'
-    },
-    {
-      id:3,
-      title: 'todo3',
-      desc: 'description3'
-    }
-  ]);
-  function addTodo (){
-    setTodos([...todos,{
-      id:count++,
-      title: Math.random(),
-      desc: Math.random()
-    }])
-  }
+  
   return (
     <>
-      <button onClick={addTodo}>Add Todo</button>
-      {todos.map((todo)=>(<Todo key={todo.id} title={todo.title} desc={todo.desc}/>))}
+    {/* this is the ugly way to pass a component inside another comp  <CardWrapper innercomp = {<TextComp/>}/> */}
+    <CardWrapper>
+      hi there 
+    </CardWrapper>
+    {/* this is the right way , and this can be accessed through "children " variable  */}
+    {/* that is CHILDREN variable contains all the things wrapped by the outer component  */}
+      </>
+  )
+}
+function TextComp(){
+  return(
+    <>
+    <h1>hello world</h1>
     </>
   )
 }
 
-function Todo({title,desc}){
+function CardWrapper({children}){
+  
   return(
-    <div>
-      <h2>{title}</h2>
-      <p>{desc}</p>
+    <div style={{border: '2px solid black', padding: '10px'}}>
+      {children}
     </div>
   )
-
 }
 
