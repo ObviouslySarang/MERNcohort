d43bb227fe1b10213efb7901d77286c35c7971ff
ObviouslySarang <2003krishnadixit@gmail.com>
2025-03-17 15:08:03 +0530

    done n dusted

--- class4-2/react-tut-vite/react - hooks/reactHooks/src/App.jsx
+++ class4-2/react-tut-vite/react - hooks/reactHooks/src/App.jsx
@@ -7,28 +7,28 @@ import React, { useState } from 'react';
   return (
     <>
     {/* this is the ugly way to pass a component inside another comp  <CardWrapper innercomp = {<TextComp/>}/> */}
-    <CardWrapper>
+    {/* <CardWrapper>
       hi there 
-    </CardWrapper>
+    </CardWrapper> */}
     {/* this is the right way , and this can be accessed through "children " variable  */}
     {/* that is CHILDREN variable contains all the things wrapped by the outer component  */}
       </>
   )
 }
-function TextComp(){
-  return(
-    <>
-    <h1>hello world</h1>
-    </>
-  )
-}
+// function TextComp(){
+//   return(
+//     <>
+//     <h1>hello world</h1>
+//     </>
+//   )
+// }
 
-function CardWrapper({children}){
+// function CardWrapper({children}){
   
-  return(
-    <div style={{border: '2px solid black', padding: '10px'}}>
-      {children}
-    </div>
-  )
-}
+//   return(
+//     <div style={{border: '2px solid black', padding: '10px'}}>
+//       {children}
+//     </div>
+//   )
+// }
 
